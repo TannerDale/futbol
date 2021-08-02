6cb2371a490768db80685db75dc91c1fda3ae366
Tanner Dale <tanner@tannerdale.com>
2021-08-02 17:21:14 -0500

    Add manager inheritance and calls to clasa variable for game teams

--- lib/seasons_manager.rb
+++ lib/seasons_manager.rb
@@ -1,6 +1,7 @@
+require_relative 'manager'
 require_relative 'percentageable'
 
-class SeasonsManager
+class SeasonsManager < Manager
   include Percentageable
 
   def coach_win_pct(season)
@@ -11,7 +12,7 @@ class SeasonsManager
   end
 
   def coach_wins(season)
-    @game_teams.reduce({}) do |acc, game|
+    @@game_teams.reduce({}) do |acc, game|
       if game.game_id[0..3] == season[0..3]
         acc[game.coach] ||= {wins: 0, total: 0}
         process_game(acc[game.coach], game)
@@ -21,7 +22,7 @@ class SeasonsManager
   end
 
   def accuracy_data(season)
-    @game_teams.reduce({}) do |acc, game|
+    @@game_teams.reduce({}) do |acc, game|
       if game.game_id[0..3] == season[0..3]
         acc[game.team_id] ||= {goals: 0, shots: 0}
         acc[game.team_id][:goals] += game.goals
@@ -32,7 +33,7 @@ class SeasonsManager
   end
 
   def team_tackles(season)
-    @game_teams.reduce({}) do |acc, game|
+    @@game_teams.reduce({}) do |acc, game|
       if game.game_id[0..3] == season[0..3]
         acc[game.team_id] ||= 0
         acc[game.team_id] += game.tackles
@@ -42,7 +43,7 @@ class SeasonsManager
   end
 
   def seasons_win_count(team_id)
-    @game_teams.reduce({}) do |acc, game|
+    @@game_teams.reduce({}) do |acc, game|
       if game.team_id == team_id
         acc[game.season] ||= {wins: 0, total: 0}
         process_game(acc[game.season], game)
@@ -51,25 +52,63 @@ class SeasonsManager
     end
   end
 
-  def get_season_game_count
-    @games.reduce({}) do |acc, game|
+  def count_of_games_by_season
+    @@games.reduce({}) do |acc, game|
       acc[game.season] ||= 0
       acc[game.season] += 1
       acc
     end
   end
 
-  def get_goals_per_season
-    @games.reduce({}) do |acc, game|
+def get_games_per_season(season)
+    @@games.count do |game|
+      game.season == season
+    end
+  end
+
+  def average_goals_by_season
+    avg_season_goals(goals_per_season)
+  end
+
+  def goals_per_season
+    @@games.reduce({}) do |acc, game|
       acc[game.season] ||= 0
       acc[game.season] += game.total_goals
       acc
     end
   end
 
-  def get_games_per_season(season)
-    @games.count do |game|
-      game.season == season
-    end
+  def season_results(id, min_max)
+    averages = season_avgs(seasons_win_count(id))
+    results = {
+      max: -> { averages.max_by { |season, avg| avg }.first },
+      min: -> { averages.min_by { |season, avg| avg }.first }
+    }
+    results[min_max].call
+  end
+
+  def coach_results(season, min_max)
+    results = {
+      max: -> { coach_win_pct(season).max_by { |coach, pct| pct }.first },
+      min: -> { coach_win_pct(season).min_by { |coach, pct| pct }.first }
+    }
+    results[min_max].call
+  end
+
+  def accuracy_results(season, min_max)
+    average = get_accuracy_avg(accuracy_data(season))
+    results = {
+      max: -> { average.max_by { |team, avg| avg }.first },
+      min: -> { average.min_by { |team, avg| avg }.first }
+    }
+    results[min_max].call
+  end
+
+  def tackle_results(season, min_max)
+    results = {
+      max: -> { team_tackles(season).max_by { |team, tackles| tackles }.first },
+      min: -> { team_tackles(season).min_by { |team, tackles| tackles }.first }
+    }
+    results[min_max].call
   end
 end
